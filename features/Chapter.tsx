1683e98c7c47edbcd5b3546ed8c20f6c3a35ee79
rickid0 <luruicheng@me.com>
2025-08-01 22:02:21 +0800

    feat(themeColor)

--- features/Chapter.tsx
+++ features/Chapter.tsx
@@ -17,8 +17,34 @@ export default function Chapter() {
   const [isShowChapterSelectionMenu, setIsShowChapterSelectionMenu] = useState(false);
   const [chapterSelectionView, setChapterSelectionView] = useState<JSX.Element[] | null>(null);
   const isFocused = useIsFocused();
+  const [themeColor, setThemeColor] = useState('');
+
+  const setThemeColorByChapterNo = (no: number) => {
+    const n = no % 6;
+    switch (n) {
+      case 0:
+        setThemeColor('bg-red');
+        break;
+      case 1:
+        setThemeColor('bg-blue');
+        break;
+      case 2:
+        setThemeColor('bg-green');
+        break;
+      case 3:
+        setThemeColor('bg-yellow');
+        break;
+      case 4:
+        setThemeColor('bg-orange');
+        break;
+      case 5:
+        setThemeColor('bg-purple');
+        break;
+    }
+  };
 
   async function start() {
+    setThemeColorByChapterNo(1);
     const db = await getDB();
     const res = await getChapter(db, 'java', 1);
     const progress = await getUnitProgress(db, 'java', '1');
@@ -76,7 +102,7 @@ export default function Chapter() {
       const isUnlocked = p.is_unlocked;
 
       const baseStyle = 'm-3 flex h-8 w-8 items-center justify-center rounded-full p-1';
-      const bgColor = isUnlocked ? 'bg-blue-400' : 'bg-gray-400';
+      const bgColor = isUnlocked ? `${themeColor}-400` : 'bg-gray-400';
       const textColor = isUnlocked ? 'text-white' : 'text-gray-300';
       return (
         <View key={p.unit_id} className={`${baseStyle} ${bgColor}`}>
@@ -89,7 +115,7 @@ export default function Chapter() {
 
   return (
     <>
-      <View className="flex-1 flex-row bg-blue-50">
+      <View className={`flex-1 flex-row  ${themeColor}-50`}>
         {isShowChapterSelectionMenu && (
           <View className="flex-1 items-center justify-center px-2 py-20">
             <View className="h-full rounded-xl bg-white">
@@ -103,7 +129,7 @@ export default function Chapter() {
 
         <View className={`${isShowChapterSelectionMenu ? 'flex-4' : 'flex-1'}`}>
           <View className={` h-36 flex-col-reverse items-center`}>
-            <View className="w-96 flex-row justify-between rounded-2xl bg-blue-200 p-2">
+            <View className={`w-96 flex-row justify-between rounded-2xl ${themeColor}-200 p-2`}>
               <View>
                 <Text className=" text-l font-menlo">
                   {chapter?.lang.toLocaleUpperCase()} Chapter {chapter?.no}
