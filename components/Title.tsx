8595edfe4cd45ef8a543d65e09e4009f3fb26c2e
rickid0 <luruicheng@me.com>
2025-08-08 20:59:12 +0800

    refactor: mv git

--- components/Title.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function Title({ children }: any) {
-  return (
-    <View className="items-center justify-center rounded bg-yellow-300 p-2">
-      <Text className="text-l font-menlo">{children}</Text>
-    </View>
-  );
-}
